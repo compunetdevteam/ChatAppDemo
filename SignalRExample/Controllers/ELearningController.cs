f75c17f9650e1c7b3b8f06138594b5f8641319a5
ioedeveloper <daviddisu8@gmail.com>
2018-02-16 10:19:15 +0100

    Birthday View E-Learning UI

---
+++ SignalRExample/Controllers/ELearningController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Web.Mvc;
+using SD = System.Drawing;
+
+namespace SignalRExample.Controllers
+{
+    public class ELearningController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Birthday()
+        {
+            return View();
+        }
+
+        public ActionResult Courses()
+        {
+            return View();
+        }
+
+        public ActionResult Modules()
+        {
+            return View();
+        }
+    }
+}
